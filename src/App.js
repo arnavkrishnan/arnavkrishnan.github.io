2cd2fa0f5c179dd5d50e78b01095995656ee2344
arnavkrishnan <80376092+arnavkrishnan@users.noreply.github.com>
2023-03-09 05:03:03 +0000

    Added chssboard

--- src/App.js
+++ src/App.js
@@ -1,15 +1,89 @@
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p className="small">
-          Hello World.
-        </p>
-      </header>
-    </div>
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p className="small">
+//           Hello World.
+//         </p>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+function Chessboard() {
+  return(
+  <div class="board">
+		<div class="light"></div>
+		<div class="dark"></div>
+		<div class="light"></div>
+		<div class="dark"></div>
+		<div class="light"></div>
+		<div class="dark"></div>
+		<div class="light"></div>
+		<div class="dark"></div>
+		<div class="dark"></div>
+		<div class="light"></div>
+		<div class="dark"></div>
+		<div class="light"></div>
+		<div class="dark"></div>
+		<div class="light"></div>
+		<div class="dark"></div>
+		<div class="light"></div>
+		<div class="light"></div>
+		<div class="dark"></div>
+		<div class="light"></div>
+		<div class="dark"></div>
+		<div class="light"></div>
+		<div class="dark"></div>
+		<div class="light"></div>
+		<div class="dark"></div>
+		<div class="dark"></div>
+		<div class="light"></div>
+		<div class="dark"></div>
+		<div class="light"></div>
+		<div class="dark"></div>
+		<div class="light"></div>
+		<div class="dark"></div>
+		<div class="light"></div>
+		<div class="light"></div>
+		<div class="dark"></div>
+		<div class="light"></div>
+		<div class="dark"></div>
+		<div class="light"></div>
+		<div class="dark"></div>
+		<div class="light"></div>
+		<div class="dark"></div>
+		<div class="dark"></div>
+		<div class="light"></div>
+		<div class="dark"></div>
+		<div class="light"></div>
+		<div class="dark"></div>
+		<div class="light"></div>
+		<div class="dark"></div>
+		<div class="light"></div>
+		<div class="light"></div>
+		<div class="dark"></div>
+		<div class="light"></div>
+		<div class="dark"></div>
+		<div class="light"></div>
+		<div class="dark"></div>
+		<div class="light"></div>
+		<div class="dark"></div>
+		<div class="dark"></div>
+		<div class="light"></div>
+		<div class="dark"></div>
+		<div class="light"></div>
+		<div class="dark"></div>
+		<div class="light"></div>
+		<div class="dark"></div>
+		<div class="light"></div>
+	</div>
   );
+
 }
 
-export default App;
+export default Chessboard
