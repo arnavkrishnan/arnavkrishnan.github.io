422b410da17b19d6973701b3666b14bcc8b9b2e1
arnavkrishnan <80376092+arnavkrishnan@users.noreply.github.com>
2023-03-10 04:27:28 +0000

    Undo Chessboard

--- src/App.js
+++ src/App.js
@@ -1,89 +1,15 @@
 import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <p className="small">
-//           Hello World.
-//         </p>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-function Chessboard() {
-  return(
-  <div class="board">
-		<div class="light"></div>
-		<div class="dark"></div>
-		<div class="light"></div>
-		<div class="dark"></div>
-		<div class="light"></div>
-		<div class="dark"></div>
-		<div class="light"></div>
-		<div class="dark"></div>
-		<div class="dark"></div>
-		<div class="light"></div>
-		<div class="dark"></div>
-		<div class="light"></div>
-		<div class="dark"></div>
-		<div class="light"></div>
-		<div class="dark"></div>
-		<div class="light"></div>
-		<div class="light"></div>
-		<div class="dark"></div>
-		<div class="light"></div>
-		<div class="dark"></div>
-		<div class="light"></div>
-		<div class="dark"></div>
-		<div class="light"></div>
-		<div class="dark"></div>
-		<div class="dark"></div>
-		<div class="light"></div>
-		<div class="dark"></div>
-		<div class="light"></div>
-		<div class="dark"></div>
-		<div class="light"></div>
-		<div class="dark"></div>
-		<div class="light"></div>
-		<div class="light"></div>
-		<div class="dark"></div>
-		<div class="light"></div>
-		<div class="dark"></div>
-		<div class="light"></div>
-		<div class="dark"></div>
-		<div class="light"></div>
-		<div class="dark"></div>
-		<div class="dark"></div>
-		<div class="light"></div>
-		<div class="dark"></div>
-		<div class="light"></div>
-		<div class="dark"></div>
-		<div class="light"></div>
-		<div class="dark"></div>
-		<div class="light"></div>
-		<div class="light"></div>
-		<div class="dark"></div>
-		<div class="light"></div>
-		<div class="dark"></div>
-		<div class="light"></div>
-		<div class="dark"></div>
-		<div class="light"></div>
-		<div class="dark"></div>
-		<div class="dark"></div>
-		<div class="light"></div>
-		<div class="dark"></div>
-		<div class="light"></div>
-		<div class="dark"></div>
-		<div class="light"></div>
-		<div class="dark"></div>
-		<div class="light"></div>
-	</div>
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="small">
+          Hello World.
+        </p>
+      </header>
+    </div>
   );
-
 }
 
-export default Chessboard
+export default App
