00d05b3477ebc0cc7a3081d81fa55a179a6c31e1
Arnav Krishnan <100034099@mvla.net>
2025-10-10 15:02:23 -0700

    responsive font size

--- src/demos/FallingTextDemo.tsx
+++ src/demos/FallingTextDemo.tsx
@@ -3,13 +3,13 @@ const FallingTextDemo = () => {
   return (
     <div className="relative w-full h-full">
       <FallingText
-        text="Hey! I'm Arnav Krishnan​. I'm currently updating this website."
+        text="Hey! I'm Arnav Krishnan. I'm currently updating this website."
         highlightWords={["Arnav", "Krishnan"]}
         trigger="hover"
         backgroundColor="transparent"
         wireframes={false}
         gravity={0.5}
-        fontSize="5rem"
+        fontSize="clamp(1rem, 2vw, 2.5rem)"
         mouseConstraintStiffness={0.7}
       />
     </div>
